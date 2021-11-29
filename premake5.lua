776b0cab7fabab8716cb9b6ea728a076cb369b84
DevilEvilSilver <longtgm1142@gmail.com>
2021-11-29 18:38:29 +0700

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project "assimp"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/assimp/**.hpp",
+		"include/assimp/**.h"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
